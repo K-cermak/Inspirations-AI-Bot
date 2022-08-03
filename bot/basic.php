a2dcbaea49699572cf6890b94c56e2aea76fa39c
EnplatedSyncer <karel4cermak@seznam.cz>
2022-08-03 23:35:34 +0200

    added twitter api

--- bot/basic.php
+++ bot/basic.php
@@ -1,10 +1,44 @@
 <?php
-    //get image properties
-    //send post request
+    require_once("tokens.php");
+    require_once("twitter/publish.php");
+
+    //send post request to URL_BASE with API_KEY and action=getId
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, URL_BASE . "?api=" . API_KEY . "&action=getId");
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($ch);
+    curl_close($ch);
+    $post = json_decode($result, true);
+
+    if ($post == "") {
+        die("Error: No posts found.");
+    } else {
+        $id = $post["id"];
+        $image = $post["fileName"];
+        $igText = $post["ig"];
+        $twitterText = $post["twitter"];
+
+        //download file from IMAGE_PATH and save it to images folder
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, IMAGE_PATH . $image);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        $result = curl_exec($ch);
+        curl_close($ch);
+        $file = fopen("twitter/images/" . $image, "w");
+        fwrite($file, $result);
+        fclose($file);
+
+        //publish tweet with image and text
+        publish($twitterText, $image);
+
+
+        //delete image
+        unlink("twitter/images/" . $image);
+    }
+
 
     //send to instagram api
-    //download image
-    //send to twitter api
+
     //wait 1 minuty
-    //delete
+    //delete request
 ?>
---
+++ bot/twitter/publish.php
@@ -0,0 +1,30 @@
+<?php
+    require_once("keys.php");
+    require_once("vendor/autoload.php");
+
+    function publish($text, $imageName) {
+        $imageLocation = __DIR__ . "/images/" . $imageName;
+
+        $credentials = array(
+            'consumer_key' => CONSUMER_KEY,
+            'consumer_secret' => CONSUMER_SECRET,
+            'token_identifier' => ACCESS_TOKEN,
+            'token_secret' => ACCESS_TOKEN_SECRET,
+        );
+
+        $twitter = new \Coderjerk\BirdElephant\BirdElephant($credentials);
+        $image = $twitter->tweets()->upload($imageLocation);
+
+        $media = (new \Coderjerk\BirdElephant\Compose\Media)->mediaIds(
+            [
+                $image->media_id_string
+            ]
+        );
+
+        echo "Tweeted: " . $text . "<br>";
+    
+        $tweet = (new \Coderjerk\BirdElephant\Compose\Tweet)->text($text)
+            ->media($media);
+        $twitter->tweets()->tweet($tweet);
+    }
+?>
