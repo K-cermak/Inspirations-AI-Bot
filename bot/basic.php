6aec621f6351f61de49f54e23b0d86563650a8da
EnplatedSyncer <karel4cermak@seznam.cz>
2022-08-03 22:21:14 +0200

    changed tokens

---
+++ bot/basic.php
@@ -0,0 +1,10 @@
+<?php
+    //get image properties
+    //send post request
+
+    //send to instagram api
+    //download image
+    //send to twitter api
+    //wait 1 minuty
+    //delete
+?>
--- page/api.php
+++ page/api.php
@@ -2,9 +2,6 @@
     header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");
-    ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
     require_once 'db.php';
     session_start();
